772b8bab231d00d5f956d357d1410abf7da08c27
Will Liu <William.Q.Liu@gmail.com>
2017-12-02 10:15:05 -0700

    Initial Setup for C++ Notes

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+/*
+Compile with: g++ -std=c++17 hello.cpp
+Run with: ./a.out
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello world!" << endl;
+
+    return 0;
+}
