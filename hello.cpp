bc47f5bb4f39aa53d8a1dd30bddd4fa524b8f954
Will Liu <William.Q.Liu@gmail.com>
2017-12-02 10:49:22 -0700

    If-else and Switch statements

--- hello.cpp
+++ hello.cpp
@@ -13,5 +13,38 @@ int main(){
 
     cout << "Hello world!" << endl;
 
+    // variable assignments
+    const double PI = 3.1415926535;
+    char myGrade = 'A';  // uses one byte of memory
+    bool isIntoxicated = false;  // same as 0
+    int myAge = 33;
+    float favNum = 3.141592;
+
+    cout << "Size of int is: " << sizeof(myAge) << endl;
+    myAge += 5;
+
+    // if-else statement
+    if((myAge >= 1) && (myAge< 16)){
+        cout << "You can't drive" << endl;
+    } else if(isIntoxicated) {
+        cout << "You can't drive you drunk" << endl;
+    } else {
+        cout << "You can drive" << endl;
+    }
+
+    // switch-statement
+    int greetingOption = 2;
+    switch(greetingOption){
+        case 1:
+            cout << "Hello" << endl;
+            break;
+        case 2:
+            cout << "Bonjour" << endl;
+            break;
+        default:
+            cout << "Halo" << endl;
+            break;
+    }
+
     return 0;
 }
